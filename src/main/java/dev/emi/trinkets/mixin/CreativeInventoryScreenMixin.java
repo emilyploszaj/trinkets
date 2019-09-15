1882ad05069d8340aa4ef864e95b225b092adefd
emilyploszaj <emilyploszaj@gmail.com>
2019-09-14 21:34:56 -0500

    Fixed type for redirect mixin, incremented version

--- src/main/java/dev/emi/trinkets/mixin/CreativeInventoryScreenMixin.java
+++ src/main/java/dev/emi/trinkets/mixin/CreativeInventoryScreenMixin.java
@@ -1,7 +1,5 @@
 package dev.emi.trinkets.mixin;
 
-import java.util.List;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -9,11 +7,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import dev.emi.trinkets.api.TrinketSlots;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.container.Slot;
+import net.minecraft.util.DefaultedList;
 
 @Mixin(CreativeInventoryScreen.class)
 public abstract class CreativeInventoryScreenMixin{
-	@Redirect(at = @At(value = "INVOKE", target = "Ljava/util/List;size()I"), method = "setSelectedTab")
-	public int getEquippedStackProxy(List<Slot> slots){
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/util/DefaultedList;size()I"), method = "setSelectedTab")
+	public int getEquippedStackProxy(DefaultedList<Slot> slots){
 		return slots.size() - TrinketSlots.getSlotCount();
 	}
 }
