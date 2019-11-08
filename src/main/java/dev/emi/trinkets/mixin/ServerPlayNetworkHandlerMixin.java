db42ee3ef620597879257c8305d83c868b4e2981
emilyploszaj <emilyploszaj@gmail.com>
2019-11-07 20:47:02 -0600

    Fixed creative slot interactions failing serverside

--- src/main/java/dev/emi/trinkets/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/dev/emi/trinkets/mixin/ServerPlayNetworkHandlerMixin.java
@@ -4,9 +4,12 @@ import dev.emi.trinkets.api.SlotGroups;
 import dev.emi.trinkets.api.Slots;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketSlots;
 import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
@@ -15,7 +18,7 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 /**
- * Makes the elytra check for flight use the trinkets chest:cape slot
+ * Makes the elytra check for flight use the trinkets chest:cape slot and modifies checked value for out of bounds creative slot indices
  */
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerPlayNetworkHandlerMixin implements ServerPlayPacketListener {
@@ -24,5 +27,10 @@ public abstract class ServerPlayNetworkHandlerMixin implements ServerPlayPacketL
 	public ItemStack getEquippedStackProxy(ServerPlayerEntity player, EquipmentSlot slot) {
 		TrinketComponent comp = TrinketsApi.getTrinketComponent(player);
 		return comp.getStack(SlotGroups.CHEST, Slots.CAPE);
+	}
+
+	@ModifyConstant(method = "onCreativeInventoryAction", constant = @Constant(intValue = 45))
+	public int modifyCreativeSlotMax(int value) {
+		return value + TrinketSlots.getSlotCount();
 	}
 }
