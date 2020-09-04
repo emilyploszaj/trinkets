9d617826751a74823163f272b5d4ea3857f38a10
TheIllusiveC4 <itwasmec4@gmail.com>
2020-09-03 19:42:43 -0700

    basic slot data loading

--- src/main/java/dev/emi/trinkets/TrinketsMain.java
+++ src/main/java/dev/emi/trinkets/TrinketsMain.java
@@ -1,11 +1,19 @@
 package dev.emi.trinkets;
 
+import dev.emi.trinkets.data.SlotLoader;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.resource.ResourceType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TrinketsMain implements ModInitializer {
-	
-	@Override
-	public void onInitialize() {
-		
-	}
+
+  public static final String MOD_ID = "trinkets";
+  public static final Logger LOGGER = LogManager.getLogger();
+
+  @Override
+  public void onInitialize() {
+    ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(SlotLoader.INSTANCE);
+  }
 }
