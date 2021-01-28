095f00adac142fa1c94356fcf549be070586201b
Florens Pauwels <github@florens.be>
2021-01-28 13:07:57 +0100

    followBodyRotations (BipedEntityModel)

--- src/main/java/dev/emi/trinkets/api/client/TrinketRenderer.java
+++ src/main/java/dev/emi/trinkets/api/client/TrinketRenderer.java
@@ -30,6 +30,33 @@ public interface TrinketRenderer {
 				float headPitch);
 
 	/**
+	 * Rotates the rendering for the models based on the entity's poses and movements. This will do
+	 * nothing if the entity render object does not implement {@link LivingEntityRenderer} or if the
+	 * model does not implement {@link BipedEntityModel}).
+	 *
+	 * @param entity The wearer of the trinket
+	 * @param model The model to align to the body movement
+	 */
+	static void followBodyRotations(final LivingEntity entity,
+									final BipedEntityModel<LivingEntity> model) {
+
+		EntityRenderer<? super LivingEntity> render = MinecraftClient.getInstance()
+				.getEntityRenderDispatcher().getRenderer(entity);
+
+		if (render instanceof LivingEntityRenderer) {
+			//noinspection unchecked
+			LivingEntityRenderer<LivingEntity, EntityModel<LivingEntity>> livingRenderer =
+					(LivingEntityRenderer<LivingEntity, EntityModel<LivingEntity>>) render;
+			EntityModel<LivingEntity> entityModel = livingRenderer.getModel();
+
+			if (entityModel instanceof BipedEntityModel) {
+				BipedEntityModel<LivingEntity> bipedModel = (BipedEntityModel<LivingEntity>) entityModel;
+				bipedModel.setAttributes(model);
+			}
+		}
+	}
+
+	/**
 	 * Translates the rendering context to the center of the player's face
 	 */
 	static void translateToFace(MatrixStack matrices, PlayerEntityModel<AbstractClientPlayerEntity> model, AbstractClientPlayerEntity player, float headYaw,
