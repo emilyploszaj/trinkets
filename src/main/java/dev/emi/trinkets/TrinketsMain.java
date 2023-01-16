d1311c1efb949af93af44ae7cd25ee8846f9eb6b
emilyploszaj <emilyploszaj@gmail.com>
2023-01-15 18:17:32 -0600

    Add trinkets command

--- src/main/java/dev/emi/trinkets/TrinketsMain.java
+++ src/main/java/dev/emi/trinkets/TrinketsMain.java
@@ -1,6 +1,20 @@
 package dev.emi.trinkets;
 
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+import static com.mojang.brigadier.arguments.StringArgumentType.string;
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+
 import dev.emi.trinkets.api.LivingEntityTrinketComponent;
+import dev.emi.trinkets.api.SlotGroup;
+import dev.emi.trinkets.api.SlotType;
+import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
 import dev.emi.trinkets.data.EntitySlotLoader;
 import dev.emi.trinkets.data.SlotLoader;
@@ -8,12 +22,16 @@ import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.command.argument.ItemStackArgument;
+import net.minecraft.command.argument.ItemStackArgumentType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.resource.ResourceType;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 public class TrinketsMain implements ModInitializer, EntityComponentInitializer {
 
@@ -27,6 +45,73 @@ public class TrinketsMain implements ModInitializer, EntityComponentInitializer 
 		resourceManagerHelper.registerReloadListener(EntitySlotLoader.SERVER);
 		ServerLifecycleEvents.END_DATA_PACK_RELOAD.register((server, serverResourceManager, success)
 				-> EntitySlotLoader.SERVER.sync(server.getPlayerManager().getPlayerList()));
+		CommandRegistrationCallback.EVENT.register((dispatcher, registry, env) -> 
+			dispatcher.register(literal("trinkets")
+				.requires(source -> source.hasPermissionLevel(2))
+				.then(
+					literal("set")
+					.then(
+						argument("group", string())
+						.then(
+							argument("slot", string())
+							.then(
+								argument("offset", integer(0))
+								.then(
+									argument("stack", ItemStackArgumentType.itemStack(registry))
+									.executes(context -> {
+										try {
+										return trinketsCommand(context, 1);
+
+										} catch (Exception e) {
+											e.printStackTrace();
+											return -1;
+										}
+									})
+									.then(
+										argument("count", integer(1))
+										.executes(context -> {
+											int amount = context.getArgument("amount", Integer.class);
+											return trinketsCommand(context, amount);
+										})
+									)
+								)
+							)
+						)
+					)
+				)
+			));
+	}
+
+	private static int trinketsCommand(CommandContext<ServerCommandSource> context, int amount) {
+		try {
+			String group = context.getArgument("group", String.class);
+			String slot = context.getArgument("slot", String.class);
+			int offset = context.getArgument("offset", Integer.class);
+			ItemStackArgument stack = context.getArgument("stack", ItemStackArgument.class);
+			ServerPlayerEntity player = context.getSource().getPlayer();
+			if (player != null) {
+				TrinketComponent comp = TrinketsApi.getTrinketComponent(player).get();
+				SlotGroup slotGroup = comp.getGroups().getOrDefault(group, null);
+				if (slotGroup != null) {
+					SlotType slotType = slotGroup.getSlots().getOrDefault(slot, null);
+					if (slotType != null) {
+						if (offset >= 0 && offset < slotType.getAmount()) {
+							comp.getInventory().get(group).get(slot).setStack(offset, stack.createStack(amount, true));
+							return Command.SINGLE_SUCCESS;
+						} else {
+							context.getSource().sendError(Text.literal(offset + " offset does not exist for slot"));
+						}
+					} else {
+						context.getSource().sendError(Text.literal(slot + " does not exist"));
+					}
+				} else {
+					context.getSource().sendError(Text.literal(group + " does not exist"));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;
 	}
 
 	@Override
