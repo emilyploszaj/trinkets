47072fb671adae744587b857eea4661d181e5906
powerboat9 <7397652+powerboat9@users.noreply.github.com>
2023-04-17 01:21:29 +0000

    Allow instances of LivingEntity to render trinket features (#248)
    * Allow zombies to wear trinkets
    
    * Apply trinket feature rendering to LivingEntity

---
+++ src/main/java/dev/emi/trinkets/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,32 @@
+package dev.emi.trinkets.mixin;
+
+import dev.emi.trinkets.TrinketFeatureRenderer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Adds the trinket feature renderer to the list of living entity features
+ *
+ * @author C4
+ * @author powerboat9
+ */
+@Environment(EnvType.CLIENT)
+@Mixin(LivingEntityRenderer.class)
+public abstract class LivingEntityRendererMixin {
+    @Invoker("addFeature")
+    public abstract boolean invokeAddFeature(FeatureRenderer feature);
+
+	@Inject(at = @At("RETURN"), method = "<init>")
+	public void init(EntityRendererFactory.Context ctx, EntityModel model, float shadowRadius, CallbackInfo ci) {
+        this.invokeAddFeature(new TrinketFeatureRenderer<>((LivingEntityRenderer) (Object) this));
+	}
+}
