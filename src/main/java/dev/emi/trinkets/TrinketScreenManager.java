b9748e9a82a4f06a71db6f89ae99cc5fbecccfb6
Aquaholic <rjja27@hotmail.com>
2026-04-08 16:11:38 +0800

    Update currently hovered trinket slot type bounds on slot update, and prevent the Recipe Book from intercepting trinket slot clicks.

--- src/main/java/dev/emi/trinkets/TrinketScreenManager.java
+++ src/main/java/dev/emi/trinkets/TrinketScreenManager.java
@@ -53,7 +53,17 @@ public class TrinketScreenManager {
 		if (group != null) {
 			if (TrinketsClient.activeType != null) {
 				if (!typeBounds.contains(Math.round(mouseX) - x, Math.round(mouseY) - y)) {
-					TrinketsClient.activeType = null;
+					// Attempt to refresh the typeBounds, in case the slot count has changed.
+					int i = handler.trinkets$getSlotTypes(group).indexOf(TrinketsClient.activeType);
+					if (i >= 0) {
+						Rect2i r = currentScreen.trinkets$getGroupRect(group);
+						Point slotHeight = handler.trinkets$getSlotHeight(group, i);
+						int height = slotHeight.y();
+						typeBounds = new Rect2i(r.getX() + slotHeight.x() - 2, r.getY() - (height - 1) / 2 * 18 - 3, 23, height * 18 + 5);
+					}
+					if (!typeBounds.contains(Math.round(mouseX) - x, Math.round(mouseY) - y)) {
+						TrinketsClient.activeType = null;
+					}
 				} else if (focusedSlot != null) {
 					if (!(focusedSlot instanceof TrinketSlot ts && ts.getType() == TrinketsClient.activeType)) {
 						TrinketsClient.activeType = null;
@@ -380,6 +390,8 @@ public class TrinketScreenManager {
 		TrinketScreen currentScreen = getCurrentScreen();
 
 		if (currentScreen != null) {
+			// Refresh the type bounds of the currently open Trinket Group on slot change.
+			typeBounds = new Rect2i(0, 0, 0, 0);
 			currentScreen.trinkets$updateTrinketSlots();
 		}
 	}
--- src/main/java/dev/emi/trinkets/mixin/RecipeBookScreenMixin.java
+++ src/main/java/dev/emi/trinkets/mixin/RecipeBookScreenMixin.java
@@ -1,7 +1,10 @@
 package dev.emi.trinkets.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.emi.trinkets.TrinketScreenManager;
 import net.minecraft.client.gui.screen.ingame.RecipeBookScreen;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,5 +17,13 @@ public class RecipeBookScreenMixin {
         if (TrinketScreenManager.isClickInsideTrinketBounds(mouseX, mouseY)) {
             info.setReturnValue(false);
         }
+    }
+
+    @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/recipebook/RecipeBookWidget;mouseClicked(DDI)Z"), method = "Lnet/minecraft/client/gui/screen/ingame/RecipeBookScreen;mouseClicked(DDI)Z")
+    private boolean overrideRecipeBookClick(RecipeBookWidget<?> instance, double mouseX, double mouseY, int button, Operation<Boolean> original) {
+        if (TrinketScreenManager.isClickInsideTrinketBounds(mouseX, mouseY)) {
+            return false;
+        }
+        return original.call(instance, mouseX, mouseY, button);
     }
 }
